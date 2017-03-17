9c7da61411eab30e96797f903938faf8428eed24
Andrew-Seo <league@WorthyMini-7.attlocal.net>
2017-03-16 19:54:55 -0700

    Snake Food

--- src/SnakesFood.java
+++ src/SnakesFood.java
@@ -6,22 +6,29 @@ import java.util.Random;
 public class SnakesFood {
 	int x = new Random().nextInt(1920);
 	int y = new Random().nextInt(960);
-	
+	SnakeModel snake;
+
 	SnakesFood() {
-		
+
 	}
 
 	public void foodSpawn(int x1, int y1) {
-		x1=x;
-		y1=y;
-		
-		
+		x1 = x;
+		y1 = y;
+
 	}
 
 	public void foodGraphics(Graphics g) {
 		g.setColor(Color.GRAY);
 		g.fillRect(x, y, 10, 10);
-		g.drawRect(x, y, 10,10);
+		g.drawRect(x, y, 10, 10);
 	}
-	
+
+	public void collision() {
+		if (snake.x == this.x && snake.y == this.y) {
+			this.x = new Random().nextInt(1920);
+			this.y = new Random().nextInt(960); 
+			//Snake body + 1
+		}
+	}
 }
