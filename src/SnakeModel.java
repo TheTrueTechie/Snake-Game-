08ff2a358c30192848e89c44ed4c4e55e4ef8682
Cole Rudnick <league@WorthyMini-7.attlocal.net>
2017-03-30 20:01:18 -0700

    Collision Detection

--- src/SnakeModel.java
+++ src/SnakeModel.java
@@ -1,20 +1,22 @@
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Rectangle;
 import java.util.ArrayList;
 import java.util.Stack;
 
 public class SnakeModel {
 	int x;
 	int y;
+	int delay = 0;
+	boolean hit = false;
 	int prevXY = 0;
 	int bufferLength = 10;
-
-	int delay = 0;
+	ArrayList<Integer> xyIndex = new ArrayList<Integer>();
+	ArrayList <Rectangle> rectangle = new ArrayList<>();
+	Rectangle body = new Rectangle(x, y, 10, 10);
 	SnakesFood food = new SnakesFood(this);
 	Stack<Integer> pastX = new Stack<Integer>();
 	Stack<Integer> pastY = new Stack<Integer>();
-
-	ArrayList<Integer> xyIndex = new ArrayList<Integer>();
 
 	public static boolean upKey = false;
 	public static boolean downKey = false;
@@ -22,10 +24,10 @@ public class SnakeModel {
 	public static boolean leftKey = false;
 
 	SnakeModel(int x, int y) {
+		xyIndex.add(0);
 		this.x = x;
 		this.y = y;
 
-		xyIndex.add(0);
 	}
 
 	public void update() {
@@ -64,21 +66,29 @@ public class SnakeModel {
 			y = 0;
 		}
 	}
+	
+	
+	public void detectCollision () {
+		for (Rectangle r: rectangle) {
+			if(r != rectangle.get(0)){
+			rectangle.get(0).intersects(r);
+			}
+			//r.setLocation(prevXY, delay);	
+		}
+	}
 
 	public void draw(Graphics g) {
-
 		pastX.add(0, x);
 		pastY.add(0, y);
-
 		delay++;
 		if (delay > bufferLength) {
 			for (int xy : xyIndex) {
 
 				g.fillRect(pastX.get(xy), pastY.get(xy), 10, 10);
-
 			}
 			pastX.pop();
 			pastY.pop();
+
 		}
 
 		g.setColor(Color.BLACK);
